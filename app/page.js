eb6aacc6dbffd8caed40b74e44059fd5aa5c0070
Sophia Banda <sophia.m.banda@gmail.com>
2025-06-04 13:50:39 -0400

    updated push event

--- app/page.js
+++ app/page.js
@@ -3,10 +3,15 @@ import "bootswatch/dist/brite/bootstrap.min.css";
 import styles from "./page.module.css";
 
 export default function Home() {
+
+const sendData = () => {
+  dataLayer.push({"event": "demoButton"});
+  console.log("pushed!")
+}
   
   return (
     <div className={styles.container}>
-      <button className="btn btn-primary" onClick={() => console.log('Clicked!')}>Request Demo</button>
+      <button className="btn btn-primary" onClick={sendData}>Request Demo</button>
     </div>
   );
 }
