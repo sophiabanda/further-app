0c8317a2d08e5ab6f9fdad97bc7d9b023ee16615
Sophia Banda <sophia.m.banda@gmail.com>
2025-06-04 14:25:02 -0400

    removed unused fonts

--- app/nav.js
+++ app/nav.js
@@ -4,10 +4,7 @@ export default function Nav() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor04" aria-controls="navbarColor04" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+    <Link className="navbar-brand" href="/">SHOP TULO</Link>
     <div className="collapse navbar-collapse" id="navbarColor04">
       <ul className="navbar-nav me-auto">
         <li className="nav-item">
@@ -15,29 +12,15 @@ export default function Nav() {
             <span className="visually-hidden">(current)</span>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
+          <Link className="nav-link" href="/Features">Features</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
+          <Link className="nav-link" href="#">Features</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-          <div className="dropdown-menu">
-            <a className="dropdown-item" href="#">Action</a>
-            <a className="dropdown-item" href="#">Another action</a>
-            <a className="dropdown-item" href="#">Something else here</a>
-            <div className="dropdown-divider"></div>
-            <a className="dropdown-item" href="#">Separated link</a>
-          </div>
+          <Link className="nav-link" href="#">Features</Link>
         </li>
       </ul>
-      <form className="d-flex">
-        <input className="form-control me-sm-2" type="search" placeholder="Search"/>
-        <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
