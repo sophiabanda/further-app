8f5f7817a846310008c51d35ed79f5e1be9c007c
Sophia Banda <sophia.m.banda@gmail.com>
2025-06-04 12:00:02 -0400

    removed unused element

--- app/page.js
+++ app/page.js
@@ -6,7 +6,6 @@ export default function Home() {
   
   return (
     <div className={styles.container}>
-      {/* <h1 className={styles.h1}>Hello world!</h1> */}
       <button className="btn btn-primary" onClick={() => console.log('Clicked!')}>Request Demo</button>
     </div>
   );
