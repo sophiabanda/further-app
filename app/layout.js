ed610ac3681e31775a5fb8b748504495997f7d58
Sophia Banda <sophia.m.banda@gmail.com>
2025-06-04 14:25:48 -0400

    spacing adjustment post-deletion that didn't save in time

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,6 @@
-// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Nav from "./nav";
 import Script from "next/script";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata = {
   title: "Further Next App",
