62cc8a4e4f4b328af7f0869f919203fad4697611
Sophia Banda <sophia.m.banda@gmail.com>
2025-06-04 11:57:31 -0400

    css fixes for uniform background color

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   
   return (
     <div className={styles.container}>
-      <h1 className={styles.h1}>Hello world!</h1>
+      {/* <h1 className={styles.h1}>Hello world!</h1> */}
       <button className="btn btn-primary" onClick={() => console.log('Clicked!')}>Request Demo</button>
     </div>
   );
