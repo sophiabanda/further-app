67a69626b235fe481f14b024108e4cc626a241a7
Sophia Banda <sophia.m.banda@gmail.com>
2025-06-12 19:29:57 -0400

    npm i to readme, target blank for easteregg

--- app/stores/page.js
+++ app/stores/page.js
@@ -1,21 +1,28 @@
-'use client'
-import "bootswatch/dist/brite/bootstrap.min.css";
-import '../cards.css'
+'use client';
+import 'bootswatch/dist/brite/bootstrap.min.css';
+import '../cards.css';
 
 export default function Stores() {
     return (
-      <div className="stores-card-container">
-        <div className="card bg-primary mb-1 card-card" style={{ maxWidth: "50vw"}}>
-          <div className="card-header">Flagship Location</div>
-          <div className="card-body">
-            <h4 className="card-title">North Carolina</h4>
-            <p className="card-text">
-              1234 Main St. #475 <br/>
-              Chapel Hill, NC 27588<br/>
-              <a href="https://lumon-industries.com/"><span style={{ fontWeight: "bold"}}>Phone:</span> 555-876-0987</a>
-            </p>
-          </div>
+        <div className="stores-card-container">
+            <div
+                className="card bg-primary mb-1 card-card"
+                style={{ maxWidth: '50vw' }}
+            >
+                <div className="card-header">Flagship Location</div>
+                <div className="card-body">
+                    <h4 className="card-title">North Carolina</h4>
+                    <p className="card-text">
+                        1234 Main St. #475 <br />
+                        Chapel Hill, NC 27588
+                        <br />
+                        <a target="_blank" href="https://lumon-industries.com/">
+                            <span style={{ fontWeight: 'bold' }}>Phone:</span>{' '}
+                            555-876-0987
+                        </a>
+                    </p>
+                </div>
+            </div>
         </div>
-      </div>
-      )
-    }
+    );
+}
