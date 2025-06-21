846be120096f0ad8ca71024f1e3e44eddf471cd9
Sophia Banda <sophia.m.banda@gmail.com>
2025-06-21 18:08:02 -0400

    WIP modal

---
+++ app/contact-modal.js
@@ -0,0 +1,9 @@
+import './contact-modal.css';
+
+export default function ContactModal() {
+    return (
+        <div className="modal-container">
+            <div className="modal"></div>
+        </div>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -1,20 +1,23 @@
-'use client'
-import "bootswatch/dist/brite/bootstrap.min.css";
-import styles from "./page.module.css";
-import Cards from "./cards";
+'use client';
+import 'bootswatch/dist/brite/bootstrap.min.css';
+import styles from './page.module.css';
+import Cards from './cards';
+import ContactModal from './contact-modal';
 
 export default function Home() {
+    const sendData = () => {
+        window.dataLayer.push({
+            event: 'demo_request',
+        });
+    };
 
-const sendData = () => {
-  window.dataLayer.push({
-    event: "demo_request"
-  });
-}
-  
-  return (
-    <div className={styles.container}>
-      <Cards/>
-      <button className="btn btn-primary" onClick={sendData}>Request Demo</button>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <ContactModal />
+            <Cards />
+            <button className="btn btn-primary" onClick={sendData}>
+                Request Demo
+            </button>
+        </div>
+    );
 }
